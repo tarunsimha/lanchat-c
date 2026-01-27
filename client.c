004306723438c88e220187d20dcfa79b33ad6c25
Tarun Simha <tarunsimha2509@gmail.com>
2026-01-27 23:44:35 +0530

    Added multiple-client handling

--- client.c
+++ client.c
@@ -14,14 +14,63 @@
 #pragma comment(lib, "ws2_32.lib")
 // For g++, use -lws2_32 compiler flag
 
-#define PORT "27015"
+#define UDP_PORT 27016
+#define PORT 27015
 #define BUFLEN 512
 
-int main(int argc, char **argv)
+DWORD WINAPI recv_thread(LPVOID arg)
+{
+    SOCKET sock = (SOCKET)arg;
+    char buf[BUFLEN];
+    memset(buf, 0, BUFLEN);
+    while (1) {
+        int n = recv(sock, buf, BUFLEN - 1, 0);
+        if (n <= 0) break;
+        buf[n] = '\0';
+        printf("\nMessage recieved: %s> ", buf);
+    }
+    return 0;
+}
+
+int main()
 {
     WSADATA wsaData;
     WSAStartup(MAKEWORD(2, 2), &wsaData);
 
+    SOCKET UDPSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+    struct sockaddr_in listenaddr;
+    struct sockaddr_in sender;
+    memset(&listenaddr, 0, sizeof(listenaddr));
+
+    listenaddr.sin_family = AF_INET;
+    listenaddr.sin_port = htons(UDP_PORT);
+    listenaddr.sin_addr.s_addr = INADDR_ANY;
+
+    bind(UDPSocket, (struct sockaddr*) &listenaddr, sizeof(listenaddr));
+
+    int senderlength = sizeof(sender);
+    memset(&sender, 0, sizeof(sender));
+
+    char udpbuffer[128];
+    int recieved = recvfrom(UDPSocket, udpbuffer, sizeof(udpbuffer) - 1, 0, (struct sockaddr*) &sender, &senderlength);
+
+    if (recieved <= 0) {
+        printf("UDP recv failed: %d\n", WSAGetLastError());
+        return 1;
+    }
+
+    udpbuffer[recieved] = '\0';
+    printf("Discovered server message: %s\n", udpbuffer);
+
+    char serverip[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &sender.sin_addr, serverip, sizeof(serverip));
+
+    printf("Server ip: %s\n", serverip);
+
+    closesocket(UDPSocket);
+
+    /*
     struct addrinfo *result = NULL;
     struct addrinfo hints;
     memset(&hints, 0, sizeof(hints));
@@ -30,7 +79,7 @@ int main(int argc, char **argv)
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_protocol = IPPROTO_TCP;
 
-    getaddrinfo(argv[1], PORT, &hints, &result);
+    getaddrinfo(serverip, PORT, &hints, &result);
 
     SOCKET ConnectSocket = INVALID_SOCKET;
     ConnectSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
@@ -43,6 +92,20 @@ int main(int argc, char **argv)
     }
     printf("Connected successfully\n");
     freeaddrinfo(result);
+    */
+
+    SOCKET ConnectSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    struct sockaddr_in server;
+    memset(&server, 0, sizeof(server));
+    server.sin_family = AF_INET;
+    server.sin_port = htons(PORT);
+    inet_pton(AF_INET, serverip, &server.sin_addr);
+
+    connect(ConnectSocket, (struct sockaddr*) &server, sizeof(server));
+    printf("Connected to chat\n");
+
+    CreateThread(NULL, 0, recv_thread, (LPVOID) ConnectSocket, 0, NULL);
 
     char buffer[BUFLEN];
 
@@ -52,12 +115,6 @@ int main(int argc, char **argv)
         fgets(buffer, BUFLEN, stdin);
 
         send(ConnectSocket, buffer, (int) strlen(buffer), 0);
-
-        int recieved = recv(ConnectSocket, buffer, BUFLEN - 1, 0);
-        if (recieved <= 0) break;
-
-        buffer[recieved] = '\0';
-        printf("Server: %s", buffer);
     }
 
     closesocket(ConnectSocket);
--- server.c
+++ server.c
@@ -10,13 +10,97 @@
 
 #pragma comment(lib, "ws2_32.lib")
 
+#define UDP_PORT 27016
 #define PORT "27015"
 #define BUFLEN 512
+#define MAX_CLIENTS 32
+
+SOCKET clients[MAX_CLIENTS];
+int client_count = 0;
+CRITICAL_SECTION client_lock;
+
+DWORD WINAPI udp_thread(LPVOID arg)
+{
+    int broadcastEnable = 1;
+    SOCKET UDPBroadcast = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    setsockopt(UDPBroadcast, SOL_SOCKET, SO_BROADCAST, (char*) &broadcastEnable, sizeof(broadcastEnable));
+
+    struct sockaddr_in broadcastaddr;
+    memset(&broadcastaddr, 0, sizeof(broadcastaddr));
+    broadcastaddr.sin_family = AF_INET;
+    broadcastaddr.sin_port = htons(UDP_PORT);
+    broadcastaddr.sin_addr.s_addr = INADDR_BROADCAST;
+
+    printf("UDP DISCOVERY BROADCAST STARTED\n");
+
+    const char* msg = "CHAT_SERVER:27015";
+    while (true) {
+        sendto(UDPBroadcast, msg, (int) strlen(msg), 0, (struct sockaddr*) &broadcastaddr, sizeof(broadcastaddr));
+        Sleep(1000);
+    }
+
+    closesocket(UDPBroadcast);
+    return 0;
+}
+
+void add_client(SOCKET s)
+{
+    EnterCriticalSection(&client_lock);
+    if (client_count < MAX_CLIENTS) clients[client_count++] = s;
+    LeaveCriticalSection(&client_lock);
+}
+
+void remove_client(SOCKET s)
+{
+    EnterCriticalSection(&client_lock);
+    for (int i = 0; i < client_count; i++) {
+        if (clients[i] == s) {
+            clients[i] = clients[--client_count];
+            break;
+        }
+    }
+    LeaveCriticalSection(&client_lock);
+}
+
+void tcp_broadcast(SOCKET sender, char *buf, int len)
+{
+    EnterCriticalSection(&client_lock);
+    for (int i = 0; i < client_count; i++) {
+        if (clients[i] != sender) {
+            send(clients[i], buf, len, 0);
+        }
+    }
+    LeaveCriticalSection(&client_lock);
+}
+
+DWORD WINAPI client_thread(LPVOID arg)
+{
+    SOCKET client = (SOCKET) arg;
+    char buffer[BUFLEN];
+
+    add_client(client);
+    printf("Client connected (%d clients)\n", client_count);
+
+    while (true)
+    {
+        int n = recv(client, buffer, BUFLEN, 0);
+        if (n <= 0) break;
+        tcp_broadcast(client, buffer, n);
+    }
+
+    remove_client(client);
+    closesocket(client);
+    printf("Client disconnected\n");
+    return 0;
+}
 
 int main()
 {
     WSADATA wsa;
     WSAStartup(MAKEWORD(2, 2), &wsa);
+
+    InitializeCriticalSection(&client_lock);
+    CreateThread(NULL, 0, udp_thread, NULL, 0, NULL);
 
     struct addrinfo hints;
     struct addrinfo* result = NULL;
@@ -39,26 +123,15 @@ int main()
     freeaddrinfo(result);
 
     listen(listenSock, SOMAXCONN);
-    printf("Server listening on port %s...\n", PORT);
+    printf("Server listening on port %s\n", PORT);
 
     while (true) {
         printf("Waiting for client\n");
         SOCKET client = accept(listenSock, NULL, NULL);
-        if (client == INVALID_SOCKET) continue;
 
-        printf("Client connected\n");
-
-        char buffer[BUFLEN];
-
-        while (true) {
-            int received = recv(client, buffer, BUFLEN, 0);
-            if (received <= 0) break;
-
-            send(client, buffer, received, 0);
+        if (client != INVALID_SOCKET) {
+            CreateThread(NULL, 0, client_thread, (LPVOID) client, 0, NULL);
         }
-
-        printf("Client disconnected\n");
-        closesocket(client);
     }
 
     closesocket(listenSock);
